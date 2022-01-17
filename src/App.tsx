abcf14bb3fa4420b876340169c64cc8bbd8a3951
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2022-01-17 10:54:45 +0530

    master

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import {
   Bar,
   XAxis,
   YAxis,
-  Tooltip,
-  Legend,
   CartesianGrid,
-  ReferenceLine
+  LabelList,
+  Cell,
+  ResponsiveContainer
 } from "recharts";
 
 
@@ -28,31 +28,49 @@ const data = [
   { sector: 'Services', perChange: -19.12, noOfStocks: 0 },
   { sector: 'Technology', perChange: -5.04, noOfStocks: 0 },
   { sector: 'Utilities', perChange: -2.52, noOfStocks: 0 }
-]
+];
 
 
 export default function App() {
   return (
-    <BarChart
-      width={1500}
-      height={500}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="sector" />
-      <YAxis />
-      <Tooltip/>
-      <Legend/>
-      <ReferenceLine y={0} stroke="#000" />
-      <Bar dataKey="perChange" fill="#8884d8" />
-      <Bar dataKey="noOfStocks" fill="#8884d8" />
-
-    </BarChart>
+    <ResponsiveContainer width={600} height={500}>
+      <BarChart
+        width={400}
+        height={500}
+        data={data}
+        layout="vertical"
+        barCategoryGap={1}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3" horizontal={true} vertical={false} />
+        <XAxis type="number" hide />
+        <YAxis dataKey="sector" type="category" width={150} />
+        <Bar
+          dataKey="perChange"
+          barSize={200}
+          fill="red"
+          radius={[0, 20, 20, 0]}
+        >
+          <LabelList
+            dataKey="perChange"
+            position="left"
+            formatter={(val:number) => (val > 0 ? `+${val}%` : `${val}%`)}
+          />
+          {data.map((entry, index) => {
+            return (
+              <Cell
+                key={`cell-${index}`}
+                fill={entry.perChange > 0 ? "#80B41C" : "#A8331F"}
+              />
+            );
+          })}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
